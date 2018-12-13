a8814a235d73b0815cb287d4a8b92c2196a99507
mrvx666 <43062346+mrvx666@users.noreply.github.com>
2018-12-13 19:06:58 +0800

    增加首次启动指定工作目录
    1.增加首次启动指定工作目录
    2.增加退出确认提示
    3.删除绘图按钮

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ Module implementing MainWindow.
 """
 from PyQt5.QtGui import QCursor
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication,QFileSystemModel,QMenu
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileSystemModel, QMenu, QMessageBox, QFileDialog
 import PyQt5.QtCore as QtCore
 from ui import Ui_MainWindow
-import sys
+import sys, os
 import numpy as np
 
-path = "./data/"
-testdataname = "Test.txt"
-testdata = path + testdataname
+defaultpathname = "data"
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -22,58 +20,73 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent=parent)
         self._initUI()
 
-    def _initUI(self):
-        print("_initUI process")
+    def _initUI(self, path=defaultpathname):
         self.setupUi(self)
         #初始化文件管理器
         self.model = QFileSystemModel()
         #设置工作目录
-        self.model.setRootPath(path)
-        self.treeView.setModel(self.model)
-        self.treeView.setRootIndex(self.model.index(path))
+        self.changworkdir(path)
+        #判断工作目录下data文件夹是否存在
+        if os.path.exists(os.getcwd()+os.path.sep+defaultpathname):
+            #存在，设置目录提示
+            self.lineEdit.setText(os.getcwd()+os.path.sep+defaultpathname)
+        else:
+            #提示用户浏览目录
+            reply = QMessageBox.warning(self, "温馨提示", "没有找到默认数据文件夹，是否浏览目录设置", QMessageBox.Yes | QMessageBox.No,
+                                        QMessageBox.Yes)
+            if (reply == QMessageBox.Yes):
+                #设置工作目录
+                self.on_browserButton_clicked()
+            if (reply == QMessageBox.No):
+                self.lineEdit.setText("请点击右侧按钮设置工作目录")
+
         #treeView右键菜单设置
         self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.treeView.customContextMenuRequested.connect(self.context_menu)
+        #窗口置顶
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
+    def changworkdir(self,path):
+        self.model.setRootPath(path)
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(self.model.index(path))
 
     def context_menu(self):
         menu = QMenu()
         open = menu.addAction("添加到绘图")
-        open.triggered.connect(self.plotDataFile)
-        clear = menu.addAction("清除绘图")
-        clear.triggered.connect(self.clear)
+        open.triggered.connect(self.plotdatafile)
         cursor = QCursor()
         menu.exec_(cursor.pos())
 
-    def plotDataFile(self):
+    def plotdatafile(self):
         index = self.treeView.currentIndex()
         file_path = self.model.filePath(index)
-        data = self.openDataFile(file_path)
+        data = self.opendatafile(file_path)
         self.pyqtgraph.plot(data)
 
-    def openDataFile(self,filepath):
+    def opendatafile(self,filepath):
         return np.loadtxt(filepath, dtype=float, skiprows=1, usecols=1)
 
-    def clear(self):
-        #清空绘图区域内容
+    @pyqtSlot()
+    def on_clearButton_clicked(self):
         self.pyqtgraph.clear()
 
     @pyqtSlot()
-    def on_pushButton_clicked(self):
-        """
-        Slot documentation goes here.
-        """
+    def on_browserButton_clicked(self):
+        fileDialog = QFileDialog()
+        fileDialog.setViewMode(QFileDialog.Detail)
+        path = QFileDialog.getExistingDirectory(self, '请选择数据文件夹', os.environ['USERPROFILE'] + os.path.sep +'desktop')
+        self.lineEdit.setText(path)
+        self.changworkdir(path)
 
-        #测试用示例数据
-        data = self.openDataFile(testdata)
-        data1 = data-0.1
+    def closeEvent(self, QCloseEvent):
+        #  推出程序确认,使用QMessageBox提示
+        reply = QMessageBox.warning(self, "温馨提示", "即将退出, 确定？", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
+        if (reply == QMessageBox.Yes):
+            QCloseEvent.accept()
+        if (reply == QMessageBox.No):
+            QCloseEvent.ignore()
 
-        #绘制图形曲线
-        self.pyqtgraph.plot(data, pen="r")
-        self.pyqtgraph.plot(data1, pen="b")
-
-    @pyqtSlot()
-    def on_pushButton_2_clicked(self):
-        self.pyqtgraph.clear()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
