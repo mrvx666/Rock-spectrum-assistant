47317f00042fcde222fb077855882a4a969fd7c3
mrvx666 <43062346+mrvx666@users.noreply.github.com>
2018-12-14 11:48:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ defaultpathname = "data"
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    plotcount = 1
+    plotlimit = 5
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
@@ -22,22 +24,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def _initUI(self, path=defaultpathname):
         self.setupUi(self)
+
         #初始化文件管理器
         self.model = QFileSystemModel()
         #设置工作目录
         self.changworkdir(path)
         #判断工作目录下data文件夹是否存在
         if os.path.exists(os.getcwd()+os.path.sep+defaultpathname):
-            #存在，设置目录提示
+            #存在
             self.lineEdit.setText(os.getcwd()+os.path.sep+defaultpathname)
         else:
             #提示用户浏览目录
             reply = QMessageBox.warning(self, "温馨提示", "没有找到默认数据文件夹，是否浏览目录设置", QMessageBox.Yes | QMessageBox.No,
                                         QMessageBox.Yes)
             if (reply == QMessageBox.Yes):
-                #设置工作目录
+                #用户点击Yes，设置工作目录
                 self.on_browserButton_clicked()
             if (reply == QMessageBox.No):
+                #用户点击No，设置提示
                 self.lineEdit.setText("请点击右侧按钮设置工作目录")
 
         #treeView右键菜单设置
@@ -46,7 +50,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         #窗口置顶
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
-    def changworkdir(self,path):
+    def changworkdir(self, path):
         self.model.setRootPath(path)
         self.treeView.setModel(self.model)
         self.treeView.setRootIndex(self.model.index(path))
@@ -61,17 +65,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         menu.exec_(cursor.pos())
 
     def plotdatafile(self):
+        if self.plotcount > self.plotlimit:
+            QMessageBox.warning(self, "温馨提示", "绘图板上已经超过" + str(self.plotlimit) + "个图形，过多绘图会导致无法分辨，请清除绘图板", QMessageBox.Yes, QMessageBox.Yes)
         index = self.treeView.currentIndex()
         file_path = self.model.filePath(index)
         try:
             data = np.loadtxt(file_path, dtype=float, skiprows=1, usecols=1)
             self.pyqtgraph.plot(data)
+            self.plotcount += 1
         except:
             QMessageBox.warning(self, "警告", "你所选择的文件不是一个数据文件", QMessageBox.Yes, QMessageBox.Yes)
 
     @pyqtSlot()
     def on_clearButton_clicked(self):
         self.pyqtgraph.clear()
+        self.plotcount = 1
 
     @pyqtSlot()
     def on_browserButton_clicked(self):
@@ -82,7 +90,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.changworkdir(path)
 
     def closeEvent(self, QCloseEvent):
-        #  推出程序确认,使用QMessageBox提示
+        #退出程序确认,使用QMessageBox提示
         reply = QMessageBox.warning(self, "温馨提示", "即将退出, 确定？", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
         if (reply == QMessageBox.Yes):
             QCloseEvent.accept()
