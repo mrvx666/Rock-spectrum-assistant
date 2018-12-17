c6bc9ce721d6195d9e90a0f8bece151c22da473e
mrvx666 <43062346+mrvx666@users.noreply.github.com>
2018-12-17 14:47:36 +0800

    检测数据目录是否存在的bug
    1.修复了启动检测目录是否存在的bug
    2.添加软件包需求列表

--- main.py
+++ main.py
@@ -35,27 +35,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.plotcount = 0
         self.plotlimit = 5
 
-    def _initUI(self, path=defaultpathname):
+    def _initUI(self):
         # 载入UI.py
         self.setupUi(self)
         # 初始化文件管理器
         self.model = QFileSystemModel()
-        # 设置工作目录
-        self.changworkdir(path)
-        # 判断工作目录下data文件夹是否存在
-        if os.path.exists(self.workdir):
+
+        # 判断程序所在目录下data文件夹是否存在
+        if os.path.isdir(self.workdir):
             # 存在，设置路径提示文本框
             self.lineEdit.setText(self.workdir)
         else:
             # 不存在，提示用户希望进行的操作
             reply = QMessageBox.warning(self, "温馨提示", "没有找到默认数据文件夹，是否浏览目录设置", QMessageBox.Yes | QMessageBox.No,
-                                        QMessageBox.Yes)
+                                        QMessageBox.No)
             if (reply == QMessageBox.Yes):
                 # 用户点击Yes，设置工作目录,模拟用户点击浏览按钮
                 self.on_browserButton_clicked()
             if (reply == QMessageBox.No):
                 # 用户点击No，设置提示
                 self.lineEdit.setText("请点击右侧按钮设置工作目录")
+
+        # 设置工作目录
+        self.changworkdir(self.workdir)
 
         # 菜单栏关联子窗体
         self.aboutwin = aboutDialogUI()
@@ -76,7 +78,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.workdir = self.model.rootPath()
         self.lineEdit.setText(self.workdir)
         # 更换目录做一次清空
-        self.pyqtgraph.clear()
+        self.on_clearButton_clicked()
 
     def context_menu(self):
         # 设定鼠标点击位置的指针
@@ -108,7 +110,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             data = np.loadtxt(self.fileindex, dtype=float, skiprows=1, usecols=1)
             self.pyqtgraph.plot(data)
             # 在状态栏上显示当前绘图的文件和绘图总数
-            self.statusbar.showMessage("plot " + self.fileindex.lstrip(self.workdir) + " ，当前绘图总数 " + str(self.plotcount+1))
+            self.statusbar.showMessage("plot " + self.fileindex.lstrip(self.workdir)
+                                       + " ，当前绘图总数 " + str(self.plotcount + 1))
             self.plotcount += 1
         except:
             QMessageBox.critical(self, "警告", "文件打开失败\n请检查数据格式", QMessageBox.Yes, QMessageBox.Yes)
@@ -183,7 +186,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         f = open(filepath, "w")
                         f.write(data)
                         f.close()
-                        self.statusbar.showMessage("write " + filepath.strip(self.workdir) + "写入成功")
+                        self.statusbar.showMessage("write " + filepath.lstrip(self.workdir) + " 写入成功")
                     except IOError as e:
                         QMessageBox.critical(self, "警告", e, QMessageBox.Yes, QMessageBox.Yes)
 
@@ -225,15 +228,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def on_clearButton_clicked(self):
         self.pyqtgraph.clear()
         self.plotcount = 0
+        self.statusbar.showMessage("workdir is " + self.workdir)
 
     @pyqtSlot()
     def on_browserButton_clicked(self):
-        fileDialog = QFileDialog()
-        fileDialog.setViewMode(QFileDialog.Detail)
-        path = QFileDialog.getExistingDirectory(self, '请选择数据文件夹', os.environ['USERPROFILE'] + os.path.sep + 'desktop')
-        self.changworkdir(path)
-        self.lineEdit.setText(path)
-        self.statusbar.showMessage("change work dir  " + path)
+            fileDialog = QFileDialog()
+            fileDialog.setViewMode(QFileDialog.Detail)
+            path = QFileDialog.getExistingDirectory(self, '请选择数据文件夹', os.environ['USERPROFILE'] + os.path.sep + 'desktop')
+            self.changworkdir(path)
+            self.lineEdit.setText(path)
+            self.statusbar.showMessage("change work dir  " + path)
 
     def about(self):
         self.aboutwin.show()
