82d6886bd7933d76abca024255b14f325033b716
mrvx666 <43062346+mrvx666@users.noreply.github.com>
2019-05-23 14:42:05 +0800

    fix findpeaks
    fuck github

--- utils/findpeaks/lib/__init__.py
+++ utils/findpeaks/lib/__init__.py
@@ -1,1 +1,3 @@
-
+from utils.findpeaks.lib.detect_peaks import detect_peaks as detect_peaks
+from utils.findpeaks.lib.findpeaks import findpeaks as Janko_Slavic_findpeaks
+from utils.findpeaks.lib.tony_beltramelli_detect_peaks import detect_peaks as tony_beltramelli_detect_peaks
